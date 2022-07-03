69734dbd43eac77eb9398505ecdf03f71f30adf8
Kim Ko <kimkolkmann@gmx.de>
2022-07-03 14:19:31 +0200

    20220703-02

--- 20220703-drag&drop1/index.php
+++ 20220703-drag&drop1/index.php
@@ -5,19 +5,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Drag & Drop</title>
-    
+    <link rel="stylesheet" href="style.css" />
+
 </head>
 <body>
     
     <div id="flexboxing">
         <div class="flexboxdiv">
-
+            <div id="MyDraggableElement" draggable="true"></div>
         </div>
         <div class="flexboxdiv">
             
         </div>
 
     </div>
+
+    <script>
+        let dragelement = document.getElementById('MyDraggableElement');
+        dragelement.addEventListener('dragstart', handleDrag);
+
+        let flexboxdiv = document.querySelectorAll('flexboxdiv');
+        flexboxdiv.addEventListener('dragover', handleDragover);
+        
+
+        function handleDrag(e)
+        {
+            dt = e.dataTransfer;
+            console.log(dt);
+            console.log(dt.types);
+            // assigning datatype and dragelement with setData
+            e.dataTransfer.setData("text/plain", dragelement.id);
+            console.log(dt);
+            console.log(dt.types);
+        }
+
+        function handleDragover(e)
+        {
+            flexboxdivdraggedover = document.getElementsByClassName('flexboxdiv');
+            flexboxdivdraggedover.classList.add('boxdragover');
+        }
+    </script>
 
 </body>
 </html>
